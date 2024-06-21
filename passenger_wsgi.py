cab39bcb3286bca991a72b9298e138423c2053e6
dboybkru <48415715+dboybkru@users.noreply.github.com>
2024-06-21 22:00:39 +0200

    pas

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 
 import os, sys
 site_user_root_dir = '/home/d/dboybklg/dboybklg.beget.tech/public_html'
-sys.path.insert(0, site_user_root_dir + '/rent_apartment')
+sys.path.insert(0, site_user_root_dir + '/rent_apartment/apartments')
 sys.path.insert(1, site_user_root_dir + '/venv/lib/python3.11/site-packages')
 os.environ['DJANGO_SETTINGS_MODULE'] = 'rent_apartment.settings'
 from django.core.wsgi import get_wsgi_application
